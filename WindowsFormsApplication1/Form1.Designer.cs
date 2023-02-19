5721878cd41a97dddac26d4b368a8b0a8129925e
Ivannikov <ivannikov.pavel1@mail.ru>
2023-02-19 13:22:57 +0300

    first commit

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.выбратьфайлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.сохранитьКакIPSToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTextBox2.Location = new System.Drawing.Point(0, 24);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(928, 528);
+            this.richTextBox2.TabIndex = 2;
+            this.richTextBox2.Text = "";
+            this.richTextBox2.WordWrap = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.выбратьфайлToolStripMenuItem,
+            this.сохранитьКакIPSToolStripMenuItem});
+            this.menuStrip1.LayoutStyle = System.Windows.Forms.ToolStripLayoutStyle.HorizontalStackWithOverflow;
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(928, 24);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // выбратьфайлToolStripMenuItem
+            // 
+            this.выбратьфайлToolStripMenuItem.Name = "выбратьфайлToolStripMenuItem";
+            this.выбратьфайлToolStripMenuItem.Size = new System.Drawing.Size(98, 20);
+            this.выбратьфайлToolStripMenuItem.Text = "Выбрать файл";
+            this.выбратьфайлToolStripMenuItem.Click += new System.EventHandler(this.выбратьфайлToolStripMenuItem_Click);
+            // 
+            // сохранитьКакIPSToolStripMenuItem
+            // 
+            this.сохранитьКакIPSToolStripMenuItem.Name = "сохранитьКакIPSToolStripMenuItem";
+            this.сохранитьКакIPSToolStripMenuItem.Size = new System.Drawing.Size(118, 20);
+            this.сохранитьКакIPSToolStripMenuItem.Text = "Сохранить как IPS";
+            this.сохранитьКакIPSToolStripMenuItem.Click += new System.EventHandler(this.сохранитьКакIPSToolStripMenuItem_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Title = "Выберете, пожалуйста файл";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(928, 552);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "GSE Handler";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem выбратьфайлToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ToolStripMenuItem сохранитьКакIPSToolStripMenuItem;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+using System.Linq;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Boolean continue_process = false;
+
+        List<byte> list_inf = new List<byte>();
+
+        String str_size_packet = "";
+        String str_size_packet_OH = "";
+
+        int size_path_packet = 0;
+        int size_path_packet_OH = 0;
+
+        int count_gl = 1;
+
+        public int set_TextBox(List<byte> byte_to_Text, int count)
+        {
+            String all_str = "";
+            all_str += count.ToString() + " фрагментированный пакет \n";
+            foreach (var a in byte_to_Text)
+            {
+                all_str += Convert.ToString(a, 16).ToUpper();
+            }
+            all_str += "\n";
+            Invoke(new Action(() => { richTextBox2.Text += all_str; }));
+            return ++count;
+        }
+
+        private void выбратьфайлToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+
+
+                using (FileStream File = new FileStream(openFileDialog1.FileName, FileMode.Open))
+                {
+                    using (BinaryReader Br = new BinaryReader(File))
+                    {
+
+                        File.Seek(9, SeekOrigin.Begin);//9 байт "IP_STREAM"
+
+                        //Считываем байты пока не достигнет конца файла
+                        while (Br.BaseStream.Position != Br.BaseStream.Length)
+                        {
+                            //--------------------------------------------------------------------------------
+                            byte[] list_ips = Br.ReadBytes(4);                                               //Заголовок IPS
+                            File.Seek(10, SeekOrigin.Current);
+                            byte[] list_gse = Br.ReadBytes(2);                                               //Заголовок GSE 2 байта которые определяют начало конец середину
+                            String str_byte = BitConverter.ToString(list_gse, 0).Replace('-'.ToString(), "");//Перевод 2-ух байтов в строковое представление в HEX-е
+                            int size_all_packet = BitConverter.ToInt32(list_ips, 0);                         //Размер всего пакета
+                                                                                                             //--------------------------------------------------------------------------------
+
+                            //Что делать если это начало пакета
+                            if (str_byte[0] == '8')
+                            {
+
+                                str_size_packet = str_byte.Remove(0, 1);
+                                size_path_packet = Convert.ToInt32(str_size_packet, 16) - 8;                    //8 байт 2-указатель на пакет и размер 6 ненужный заголовок
+                                File.Seek(6, SeekOrigin.Current);                                               //Пропуск ненужных байт
+                                list_inf.AddRange(Br.ReadBytes(size_path_packet));
+                                File.Seek(4, SeekOrigin.Current);                                               //Пропуск ненужных байт
+
+                                continue_process = true;
+
+                            }
+                            else if (str_byte[0] == '6' && continue_process)
+                            {
+
+                                str_size_packet_OH = str_byte.Remove(0, 1);
+                                size_path_packet = Convert.ToInt32(str_size_packet_OH, 16) - 4;
+                                list_inf.AddRange(Br.ReadBytes(size_path_packet - 2));
+                                File.Seek(4, SeekOrigin.Current);
+                                size_path_packet_OH += size_path_packet + 14;
+
+                                while (size_all_packet != size_path_packet_OH)
+                                {
+                                    byte[] packet_OH;
+                                    packet_OH = Br.ReadBytes(2);
+                                    String str_byte_OH = BitConverter.ToString(packet_OH).Replace('-'.ToString(), "");
+                                    str_size_packet_OH = "";
+                                    size_path_packet_OH += 2;
+
+                                    if (str_byte_OH[0] == 'C' && size_all_packet != size_path_packet_OH + 2)
+                                    {
+                                        str_size_packet_OH = str_byte_OH.Remove(0, 1);
+                                        int size_prop = Convert.ToInt32(str_size_packet_OH, 16) - 2;
+
+                                        if (size_path_packet_OH + size_prop + 4 == size_all_packet)
+                                        {
+                                            File.Seek(size_prop, SeekOrigin.Current);
+                                            File.Seek(4, SeekOrigin.Current);
+                                            size_path_packet_OH += size_prop + 4;
+                                        }
+                                        else
+                                        {
+                                            File.Seek(size_prop, SeekOrigin.Current);
+                                            size_path_packet_OH += size_prop;
+                                        }
+                                    }
+                                    else if (str_byte_OH[0] == '2' && size_all_packet != size_path_packet_OH + 2)
+                                    {
+                                        str_size_packet_OH = str_byte_OH.Remove(0, 1);
+                                        int size_prop = Convert.ToInt32(str_size_packet_OH, 16) - 2;
+                                        //File.Seek(size_prop, SeekOrigin.Current);
+                                        list_inf.AddRange(Br.ReadBytes(size_prop));
+                                        size_path_packet_OH += size_prop;
+                                    }
+                                    else if (str_byte_OH[0] == '8' && size_all_packet != size_path_packet_OH + 2)
+                                    {
+                                        str_size_packet_OH = str_byte_OH.Remove(0, 1);
+                                        int size_prop = Convert.ToInt32(str_size_packet_OH, 16) - 2;
+                                        //list_inf.Clear();
+                                        list_inf.AddRange(Br.ReadBytes(size_prop));
+                                        File.Seek(4, SeekOrigin.Current);
+                                        continue_process = true;
+                                        size_path_packet_OH += size_prop + 4;
+                                        count_gl = set_TextBox(list_inf, count_gl);
+                                    }
+                                    else if (str_byte_OH[0] == '6' && size_all_packet != size_path_packet_OH + 2)
+                                    {
+                                        str_size_packet_OH = str_byte_OH.Remove(0, 1);
+                                        size_path_packet = Convert.ToInt32(str_size_packet_OH, 16) - 2;
+                                        if (size_path_packet_OH + size_path_packet + 4 == size_all_packet)
+                                        {
+                                            File.Seek(size_path_packet, SeekOrigin.Current);
+                                            File.Seek(4, SeekOrigin.Current);//CRC-32
+                                            size_path_packet_OH += size_path_packet + 4;
+                                        }
+                                        else
+                                        {
+                                            File.Seek(size_path_packet, SeekOrigin.Current);
+                                            size_path_packet_OH += size_path_packet;
+                                        }
+                                    }
+                                    else if (str_byte_OH[0] == '6' && size_all_packet != size_path_packet_OH + 2 && continue_process)
+                                    {
+                                        str_size_packet_OH = str_byte_OH.Remove(0, 1);
+                                        size_path_packet = Convert.ToInt32(str_size_packet_OH, 16) - 2;
+                                        if (size_path_packet_OH + size_path_packet + 4 == size_all_packet)
+                                        {
+                                            File.Seek(size_path_packet, SeekOrigin.Current);
+                                            File.Seek(4, SeekOrigin.Current);//CRC-32
+                                            size_path_packet_OH += size_path_packet + 4;
+                                        }
+                                        else
+                                        {
+                                            File.Seek(size_path_packet, SeekOrigin.Current);
+                                            size_path_packet_OH += size_path_packet;
+                                        }
+                                    }
+                                    else
+                                    {
+                                        File.Seek(2, SeekOrigin.Current);
+                                        size_path_packet_OH += 2;
+                                    }
+                                }
+                                continue_process = false;
+                                size_path_packet_OH = 0;
+
+                                if (list_inf.Count != 0)
+                                {
+                                    count_gl = set_TextBox(list_inf, count_gl);
+                                    list_inf.Clear();
+                                }
+
+                            }
+                            else
+                            {
+                                int size_else = BitConverter.ToInt32(list_ips, 0);
+                                File.Seek(size_else - 12, SeekOrigin.Current);
+                                continue_process = false;
+                            }
+                        }
+                    }
+                }
+
+
+            }
+            MessageBox.Show("Файл обработан!");
+        }
+
+        private void сохранитьКакIPSToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            SaveFileDialog save_file = new SaveFileDialog();
+            save_file.Title = "Выберете место, куда хотите сохранить файл: ";
+            save_file.Filter = "files (*.ips)|*.ips";
+            save_file.DefaultExt = ".ips";
+
+            if (save_file.ShowDialog() == DialogResult.OK)
+            {
+                if (richTextBox2.Text != "")
+                {
+
+
+                    Stream st = new FileStream(save_file.FileName, FileMode.Create);
+                    List<string> stroki = new List<string>();
+                    string str_save = "";
+                    int digit = 0;
+                    string IP_STREAM = "IP_STREAM";
+                    byte[] bt;
+                    stroki.AddRange(richTextBox2.Text.Split('\n'));
+
+                    using (BinaryWriter bw = new BinaryWriter(st))
+                    {
+                        foreach (char str_ip in IP_STREAM)
+                        {
+
+                            bw.Write(Convert.ToByte(Convert.ToInt32(str_ip)));
+
+                        }
+
+                        for (int i = 1; i < stroki.Count; i += 2)
+                        {
+                            bt = BitConverter.GetBytes(Convert.ToInt32(stroki[i].Length) / 2);
+                            bw.Write(bt);
+
+                            for (int j = 0; j < stroki[i].Length - 1; j += 2)
+                            {
+
+                                str_save += $"{stroki[i][j]}{stroki[i][j + 1]}";
+                                bw.Write(Convert.ToByte(Convert.ToInt32(str_save, 16)));
+                                str_save = "";
+                            }
+
+                        }
+
+                    }
+                    MessageBox.Show("Файл сохранен!");
+                }
+                else
+                {
+                    MessageBox.Show("Вы пытаетесь сохранить нулевые данные!");
+                }
+            }
+        }
+    }
+}
